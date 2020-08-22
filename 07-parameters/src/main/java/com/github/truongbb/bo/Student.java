738499bbe73da9454e7c861033c6b835192f3f37
TRUONGBB\truon <truongbb96@outlook.com>
2020-08-22 10:24:45 +0700

    ref: lesson 07

---
+++ 07-parameters/src/main/java/com/github/truongbb/bo/Student.java
@@ -0,0 +1,35 @@
+package com.github.truongbb.bo;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "student")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Student {
+
+  @Id
+  @Column(name = "ID")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GEN_ID_STUDENT")
+  @SequenceGenerator(name = "GEN_ID_STUDENT", sequenceName = "SEQ_STUDENT", allocationSize = 1)
+  private Long id;
+
+  @Column(name = "fullname", nullable = false)
+  private String fullName;
+
+  @Column(name = "birthday", nullable = false)
+  private Date birthday;
+
+  @Column(name = "classname", nullable = false)
+  private String className;
+
+}
